cdb6634636e692478de723e81786c1b798c32b2a
Ujjwal Kirti <kirti.chargers@gmail.com>
2025-06-16 18:24:37 +0530

    bugfix: fix build errors

--- utils/functions/image/index.ts
+++ utils/functions/image/index.ts
@@ -1,5 +1,8 @@
 export const imgSrcModifier = (text: string) => {
-    const txt = document.createElement("textarea");
-    txt.innerHTML = text;
-    return txt.value;
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+        const txt = document.createElement("textarea");
+        txt.innerHTML = text;
+        return txt.value;
+    }
+    return text;
 };
