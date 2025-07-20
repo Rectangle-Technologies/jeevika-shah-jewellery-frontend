c27225020baf9007473e6491615108cecb9ecc18
Naman Khater <naman22khater@gmail.com>
2025-07-20 16:26:39 +0530

    Changed loder color

--- components/checkout/ProcessingPayment.tsx
+++ components/checkout/ProcessingPayment.tsx
@@ -6,7 +6,7 @@ function ProcessingPayment() {
 		<div className="fixed bg-white/35 backdrop-blur-sm z-50 top-0 left-0 h-screen w-screen flex flex-col gap-4 items-center justify-center">
             <Image src="/assets/logo-secondary.png" alt="loading" width={100} height={100} />
 			<div className="loader"></div>
-			<p className="text-black">Processing payment details! Please wait.</p>
+			<p className="text-black">Processing order! Please wait.</p>
 		</div>
 	);
 }
