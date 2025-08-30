4eea421604d73d1b65de9100d4559d630af59819
Naman Khater <naman22khater@gmail.com>
2025-08-30 10:45:56 +0530

    Added analytics

--- app/(breadcrumb-pages)/layout.tsx
+++ app/(breadcrumb-pages)/layout.tsx
@@ -1,10 +1,12 @@
 import { Breadcrumbs } from "@/components/common/BreadCrumbs";
+import { Analytics } from '@vercel/analytics/next';
 
 export default function BreadcrumbLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<section className="min-h-[calc(100vh-64px)] pt-28 md:pt-36 w-full px-2 md:w-[95%] mx-auto">
 			<Breadcrumbs />
 			{children}
+			<Analytics />
 		</section>
 	);
 }
